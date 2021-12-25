8fefc90d956a7d2cdabd4fad459039d7a9c2d7d6
troethe <thomas@43t.de>
2021-12-25 12:00:09 +0100

    Add doc string

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 from sqlalchemy import create_engine
 
 from .models import SolutionInformation, SolutionInformationInput, PastComputations
@@ -21,7 +21,16 @@ async def init() -> None:
 
 @app.get("/api/solutions/user/{user_id}/", response_model=PastComputations)
 @app.get("/api/solutions/user/{user_id}", include_in_schema=False, response_model=PastComputations)
-def get_computations(user_id: str):
+def get_computations(user_id: str, http_req: Request):
+    """
+    Given a user, returns meta information about past computation runs
+    """
+    userId = http_req.headers.get("UserId")
+    role = http_req.headers.get("Role")
+
+    if userId != user_id and role != "admin":
+        raise HTTPException(status_code=401)
+
     with Session() as session:
         solutions = session.query(Solution).filter_by(user_id=user_id)
 
@@ -39,7 +48,11 @@ def get_computations(user_id: str):
 
 @app.post("/api/solutions/upload/", include_in_schema=False)
 @app.post("/api/solutions/upload", include_in_schema=False)
-def add_solution(solution_request: SolutionInformationInput):
+def add_solution(solution_request: SolutionInformationInput, http_req: Request):
+    role = http_req.headers.get("Role")
+
+    if role != "admin":
+        raise HTTPException(status_code=401)
 
     if solution_request.body:
         file_uuid = drop_file(solution_request.body, solution_request.user_id, solution_request.computation_id)
